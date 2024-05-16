e126826e56d1456c60585f114f45e076948f5fa2
Piotr Węgrzyn <piotrwegrzyn@protonmail.com>
2024-05-16 16:49:03 +0200

    Fixes

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM golang:alpine AS build
 
 WORKDIR /
 COPY wait_for_ipfs.go wait_for_ipfs.go
-RUN CGO_ENABLED=0 go build wait_for_ipfs.go -o ipfs_waiter
+RUN CGO_ENABLED=0 go build -o ipfs_waiter wait_for_ipfs.go
 
 FROM ipfs/go-ipfs
 
 USER root
 
-COPY --from=build ipfs_waiter /bin/ipfs_waiter
+COPY --from=build ipfs_waiter /ipfs_waiter
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT [ "/bin/sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # IPFS startup
 ipfs init
 ipfs daemon &
-ipfs_waiter
+./ipfs_waiter
 
 # Pinning files
 ipfs pin remote service add $REMOTE_SERVICE_NAME $REMOTE_SERVICE_ENDPOINT $REMOTE_SERVICE_KEY
@@ -11,13 +11,13 @@ CID=$(ipfs add -r -Q --cid-version=1 $UPLOAD_PATH)
 ipfs pin remote add --service=$REMOTE_SERVICE_NAME --background=true $CID
 
 # Import publish key
-echo $PUBLISH_KEY_VALUE >> $PUBLISH_KEY_NAME.pam
-ipfs key import --format=pem-pkcs8-cleartext $PUBLISH_KEY_NAME $PUBLISH_KEY_NAME.pam
+echo $PUBLISH_KEY_VALUE > $PUBLISH_KEY_NAME.pem
+ipfs key import --format=pem-pkcs8-cleartext $PUBLISH_KEY_NAME $PUBLISH_KEY_NAME.pem
 
 # Unpin previous hash from remote  
 IPNS=$(ipfs key list -l | head -2 | tail -1 | awk '{printf $1}')
 OLD_CID=$(ipfs name resolve $IPNS | sed 's/\/ipfs\///')
-ipfs pin remote rm --service=$REMOTE_SERVICE_NAME $OLD_CID
+ipfs pin remote rm --service=$REMOTE_SERVICE_NAME --cid=$OLD_CID
 
 # Publish new CID to IPNS
 ipfs name publish --key=$PUBLISH_KEY_NAME $CID
